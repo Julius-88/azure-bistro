c8f00d9d7fc33ff2753ca590e52cb93eb46ecf72
Julius Alamarvdashti <drako886@gmail.com>
2023-12-19 11:02:48 +0000

    Change the link to see if it works in heroku

--- azure_bistro/settings.py
+++ azure_bistro/settings.py
@@ -148,7 +148,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_LOCATION = 'static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
