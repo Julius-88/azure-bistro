ec2ab0657f396e7b15d6d7553034d7acf581d55c
Julius Alamarvdashti <drako886@gmail.com>
2023-12-20 21:33:39 +0000

    Create reservation model and register in admin

--- azure_bistro/settings.py
+++ azure_bistro/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'cloudinary',
     'home',
+    'reservation',
 ]
 
 MIDDLEWARE = [
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+TIME_CHOICES = [
+    ('18:00', '18:00'),
+    ('19:30', '19:30'),
+    ('20:00', '20:00'),
+    ('20:30', '20:30'),
+    ('21:00', '21:00'),
+    ('21:30', '21:30'),
+    ('22:00', '22:00'),
+]
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    number_of_people = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(4)
+        ]
+    )
+    reservation_date = models.DateField()
+    reservation_time = models.TimeField(choices=TIME_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (
+            f'Reservation for {self.number_of_people} people on '
+            f'{self.reservation_date} at {self.reservation_time} '
+            f'by {self.user.username}'
+        )
