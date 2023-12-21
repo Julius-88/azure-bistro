5da536807537fba0bdf14c56c26b51adb2654046
Julius Alamarvdashti <drako886@gmail.com>
2023-12-21 18:21:03 +0000

    Add reservations made to users manage reservations page

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('reservation/', views.reservation, name='reservation'),
+    path(
+        'manage_reservation/',
+        views.manage_reservation,
+        name='manage_reservation'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ReservationForm
+from .models import Reservation
+from django.contrib import messages
 
 
 def reservation(request):
@@ -9,8 +11,18 @@ def reservation(request):
             reservation = form.save(commit=False)
             reservation.user = request.user
             reservation.save()
+            messages.success(request, 'Reservation made successfully!')
             return redirect('home')
     else:
         form = ReservationForm()
 
     return render(request, 'reservation/reservation.html', {'form': form})
+
+
+def manage_reservation(request):
+    user_reservations = Reservation.objects.filter(user=request.user).order_by(
+        '-created_at')
+    return render(
+        request,
+        'reservation/manage_reservation.html',
+        {'reservations': user_reservations})
